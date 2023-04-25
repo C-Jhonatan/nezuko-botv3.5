ba856013953d4b39a33df64aa98c12432f940280
ⱼₕₒₙₐₜₐₙ <123085363+C-Jhonatan@users.noreply.github.com>
2023-04-24 19:39:07 -0500

    nezuko-bot

---
+++ funções de cmd/menu/infodono.js
@@ -0,0 +1,12 @@
+const infodono = (prefix) => { 
+
+return ` 
+╭═•ೋ•𝐈𝐍𝐅𝐎✰𝑴𝑨𝑬𝑺𝑻𝑹𝑶•ೋ•══╮
+╎│⋟ 𝑫𝒖𝒆ñ𝒐 Jhonatan
+╎│⋟ 𝑵𝒖́𝒎𝒆𝒓𝒐⧽ wa.me/${numerodn}
+╎│⋟ 𝑷𝒓𝒆𝒇𝒊𝒙𝒐『 ${prefix} 』
+╰════•ೋ•✧๑•๑✧•ೋ•═════╯
+`
+}
+
+exports.infodono = infodono
